6ee617e5010debc9d637323060448a8334b0d17d
selman <selman.alp@hotmail.com.tr>
2019-10-16 02:02:39 +0200

    Homework Update

--- Homework1/millionaire/myservice/views/quizzes.py
+++ Homework1/millionaire/myservice/views/quizzes.py
@@ -7,63 +7,81 @@ quizzes = JsonBlueprint('quizzes', __name__)
 _LOADED_QUIZZES = {}  # list of available quizzes
 _QUIZNUMBER = 0  # index of the last created quizzes
 
-@quizzes.route("/quizzes")
+
+@quizzes.route("/quizzes", methods=['GET', 'POST'])
 def all_quizzes():
     if 'POST' == request.method:
-       return create_quiz(request)
+        return create_quiz(request)
     elif 'GET' == request.method:
-        return get_all_quizzes()
+        return get_all_quizzes(request)
 
 
 @quizzes.route("/quizzes/loaded", methods=['GET'])
-def loaded_quizzes():  # returns the number of quizzes currently loaded in the system
-    return jsonify({"Number of quizzes ":_LOADED_QUIZZES})
+def loaded_quizzes():
+    return jsonify({"loaded_quizzes": len(_LOADED_QUIZZES)})
 
-# TODO: complete the decoration
-@quizzes.route("/quiz/<id>")
+
+@quizzes.route("/quiz/<id>", methods=['GET', 'DELETE'])
 def single_quiz(id):
     global _LOADED_QUIZZES
     result = ""
 
-    if exists_quiz(id):
-        if 'GET' == request.method:  
-            result = jsonify({"Quiz":_LOADED_QUIZZES[id]})
-        
-        elif 'DELETE' == request.method:
-            # TODO: delete a quiz and get back number of answered questions
-            # and total number of questions
-             result = jsonify({"number of answered questions ":_LOADED_QUIZZES[id].currentQuestion,"total number of questions ": len(_LOADED_QUIZZES[id].questions)})
+    exists_quiz(id)
+
+    if 'GET' == request.method:
+        result = jsonify(_LOADED_QUIZZES[id].serialize())
+    elif 'DELETE' == request.method:
+        result = jsonify({"answered_questions": _LOADED_QUIZZES[id].currentQuestion,
+                          "total_questions": len(_LOADED_QUIZZES[id].questions)})
+        del _LOADED_QUIZZES[id]
     return result
 
 
-# TODO: complete the decoration
 @quizzes.route("/quiz/<id>/question", methods=['GET'])
 def play_quiz(id):
     global _LOADED_QUIZZES
-    
-    # TODO: check if the quiz is an existing one
-    if exists_quiz(id):
-        if 'GET' == request.method:  
-            # TODO: retrieve next question in a quiz, handle exceptions
-            return _LOADED_QUIZZES[id].getQuestion()
-     
+    result = ""
+    try:
+        exists_quiz(id)
+    except NonExistingAnswerError as e:
+        result = e.value
+
+    if 'GET' == request.method:
+        try:
+            result = jsonify(_LOADED_QUIZZES[id].getQuestion())
+        except CompletedQuizError as e:
+            result = jsonify({"msg": "completed quiz"})
+        except LostQuizError as e2:
+            result = jsonify({"msg": str(e2)})
+
+    return result
 
 
-# TODO: complete the decoration
 @quizzes.route("/quiz/<id>/question/<answer>", methods=['PUT'])
 def answer_question(id, answer):
     global _LOADED_QUIZZES
     result = ""
     # TODO: check if the quiz is an existing one
-    quiz = _LOADED_QUIZZES[id]
-    
-    # TODO: check if quiz is lost or completed and act consequently
+    try:
+        exists_quiz(id)
+        quiz = _LOADED_QUIZZES[id]
+    except NonExistingAnswerError as e:
+        result = e.value
 
-    if 'PUT' == request.method:  
-
-        # TODO: Check answers and handle exceptions 
-
+    if 'PUT' == request.method and not quiz.isCompleted() and not quiz.isLost():
+        # TODO: Check answers and handle exceptions
+        try:
+            result = quiz.checkAnswer(givenAnswer=answer)
+        except CompletedQuizError as e:
+                if len(quiz.questions) == quiz.currentQuestion-1:
+                    result = "you won 1 million clams!"
+                else:
+                    result = str(e)
+        except LostQuizError as e2:
+                result = str(e2)
+        print("answer : {} ".format(answer))
         return jsonify({'msg': result})
+
 
 ############################################
 # USEFUL FUNCTIONS BELOW (use them, don't change them)
@@ -98,5 +116,5 @@ def get_all_quizzes(request):
 def exists_quiz(id):
     if int(id) > _QUIZNUMBER:
         abort(404)  # error 404: Not Found, i.e. wrong URL, resource does not exist
-    elif not(id in _LOADED_QUIZZES):
+    elif not (id in _LOADED_QUIZZES):
         abort(410)  # error 410: Gone, i.e. it existed but it's not there anymore
